fdcbe4e498ea8a05512f64cdb574fe6e3ad7b443
tatmos <cha@bb.wakwak.com>
2015-11-01 11:59:00 +0900

    3,4回に一回くらい繋ぎが悪いことがあるので、delayを追加。

--- intro_loop/Assets/BGMController.cs
+++ intro_loop/Assets/BGMController.cs
@@ -5,6 +5,7 @@ public class BGMController : MonoBehaviour {
 
 	public AudioClip intro;
 	public AudioClip loop;
+	public float delay = 0.1f;
 
 	AudioSource[] audioSource = new AudioSource[2];
 
@@ -16,12 +17,12 @@ public class BGMController : MonoBehaviour {
 		if(intro != null){
 			audioSource[0].playOnAwake = false;
 			audioSource[0].clip = intro;
-			audioSource[0].Play();
+			audioSource[0].PlayScheduled(AudioSettings.dspTime + delay);
 			if(loop != null){
 				audioSource[1].playOnAwake = false;
 				audioSource[1].clip = loop;
 				audioSource[1].loop = true;
-				audioSource[1].PlayScheduled(AudioSettings.dspTime + intro.length);
+				audioSource[1].PlayScheduled(AudioSettings.dspTime + delay + intro.length);
 			}
 		}
 	}
