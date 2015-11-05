71e1ca5d76608e1b407e765aeee3b6842a7178b6
tatmos <cha@bb.wakwak.com>
2015-11-06 08:28:32 +0900

    調整

--- intro_loop/Assets/TimingController.cs
+++ intro_loop/Assets/TimingController.cs
@@ -35,14 +35,16 @@ public class TimingController : MonoBehaviour {
 	}
 	void Update()
 	{
-		if(click != null && AudioSettings.dspTime > nextScheduledTime-interval){
+		if(click != null && AudioSettings.dspTime > nextScheduledTime-(interval/2)){
 			audioSource[playerIndex%2].playOnAwake = false;
-			audioSource[playerIndex%2].clip = click;
+			//audioSource[playerIndex%2].clip = click;
 			audioSource[playerIndex%2].PlayScheduled(nextScheduledTime);
+			double tmpTime = nextScheduledTime;
 			playerIndex++;
 			lastSetupTime = AudioSettings.dspTime;
 			nextScheduledTime = AudioSettings.dspTime + interval + interval;
-			Debug.Log(string.Format("count : {0} Start dsp timing : {1,4:F3} delay : {2,4:F3} nextScheduledTime {3,4:F3} delta : {4,4:F3} interval : {5,4:f3}", playerIndex,AudioSettings.dspTime , delay , nextScheduledTime ,Time.deltaTime,interval));
+			Debug.Log(string.Format("[count] {0} dspTime : {1,4:F3} [delay] {2,4:F3} [next] {3,4:F3} [deltaTime] {4,4:F4} [difference] {5,4:f4} [interval] {6,4:f4} ", 
+			                        playerIndex,	AudioSettings.dspTime , delay , nextScheduledTime ,Time.deltaTime,AudioSettings.dspTime - tmpTime,interval));
 		}
 	}
 }
